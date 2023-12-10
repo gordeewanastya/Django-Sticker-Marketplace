d5628fd4312763ca3a94bf53d8d24ca07b9768a8
Nastya Gordeeva <nastyagordeeva72@gmail.com>
2023-12-11 00:34:54 +0200

    M-Added-logout-functionality

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 from core.forms import LoginForm
@@ -9,5 +10,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm,template_name='core/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm, template_name='core/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
